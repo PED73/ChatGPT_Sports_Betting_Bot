52d9182496481df75076ffb4b24459a2893b69b9
PED73 <162934429+PED73@users.noreply.github.com>
2024-03-16 10:08:15 +1100

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -1,22 +1,19 @@
-// Replace with your own firebase config!
-export const FIREBASE_CONFIG = {
- apiKey: "AIzaSyB55OOWTWROQumWL84P79txhwD2HspvSZs",
-  authDomain: "gpt-wager.firebaseapp.com",
-  projectId: "gpt-wager",
-  storageBucket: "gpt-wager.appspot.com",
-  messagingSenderId: "380002747460",
-  appId: "1:380002747460:web:7f3191826cc2c84eaf6811",
-  measurementId: "G-YG2GY8D0FB"
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-// Replace with your own IDs! follow the guides here:
-// https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
-export const ANDROID_GOOGLE_CLIENT_ID =
-  '878215484396-4pbcf3tghqe9sa0pvq3fifb5etnn4q6s.apps.googleusercontent.com'
-export const IOS_GOOGLE_CLIENT_ID =
-  '878215484396-5hiqfjetgvbdck03l0jepdgku6u64erm.apps.googleusercontent.com'
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAobDniT9UQdRFaxIv-K0vZt6islVFgSJw",
+  authDomain: "bet-bot-copy.firebaseapp.com",
+  projectId: "bet-bot-copy",
+  storageBucket: "bet-bot-copy.appspot.com",
+  messagingSenderId: "36750563870",
+  appId: "1:36750563870:web:18bc7627108413e392ac0f",
+  measurementId: "G-K4T8JKBSGQ"
+};
 
-// Replace with your own facebook app ID.
-// You can find more information how to generate one here:
-// https://docs.expo.io/versions/latest/sdk/facebook/#registering-your-app-with-facebook
-export const FACEBOOK_APP_ID = '820370405209413'
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
